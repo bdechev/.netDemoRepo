3afeb5017802d79aade1dda81abbda4fd8c7d6b9
FOURTH\BDechev <borislav.dechev@fourth.com>
2016-11-24 17:33:03 +0200

    upload multilanguage project

---
+++ MultiLanguage.NET/MultiLanguage/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiLanguage.Models.Models;
+
+namespace MultiLanguage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var lang = LanguageHelper.GetCurrentLanguage();
+            if(lang == Language.BG)
+                ViewBag.Message = "Language is Bulgarian";
+            else if(lang == Language.EN)
+                ViewBag.Message = "Language is English";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MultiLanguage.NET/MultiLanguage/Models/Models/LanguageHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiLanguage.Models.Models
+{
+    public enum Language
+    {
+        BG = 0,
+        EN = 1
+    }
+
+    public static class LanguageHelper
+    {
+        public static Language GetCurrentLanguage()
+        {
+            var cookie = HttpContext.Current.Request.Cookies["Language"];
+            if (cookie != null && cookie.Value != null)
+            {
+                switch (cookie.Value)
+                {
+                    case "bg":
+                        return Language.BG;
+                    case "en":
+                        return Language.EN;
+                    default:
+                        return Language.EN;
+                }
+            }
+
+            return Language.EN;
+        }
+
+    }
+}
